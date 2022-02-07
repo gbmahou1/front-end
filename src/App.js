9b7f589960cd1b1c9d18c1a0112ccf31da8babdb
gbmahou1 <gbmahou1@hotmail.com>
2022-02-07 16:16:50 -0300

    nova versão

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from './routes/landing_page';
+import RegisterPage from './routes/register_page';
 
 function App() {
   return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<PaginaPrincipal />}></Route>
-				<Route path="/contato" element={<Contato />}></Route>
+				<Route path="/" element={<LandingPage />}></Route>
+				<Route path="/register" element={<RegisterPage />}></Route>
 			</Routes>
 		</BrowserRouter>
   );
